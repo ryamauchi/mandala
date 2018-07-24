b4006f60efaa42eb8169d8edd409ca8e38320704
Ubuntu <ubuntu@ip-172-31-31-137.us-west-2.compute.internal>
2018-07-24 09:32:05 +0000

    add to_gpu/cpu

--- mandala/__init__.py
+++ mandala/__init__.py
@@ -1,6 +1,8 @@
 from mandala import basic_math
 from mandala import get_item
+from mandala import cuda
 
 
 basic_math.install_node_arithmetics()
 get_item.install_node_get_item()
+cuda.install_variable_cuda()
--- mandala/cuda.py
+++ mandala/cuda.py
@@ -1,6 +1,8 @@
 import numpy
+from mandala.nodecore import Variable
+
 try:
-    import cupy    
+    import cupy
     available = True
 except:
     available = False
@@ -14,3 +16,32 @@ def get_array_module(*args):
         return numpy
     else:
         return numpy
+
+
+def to_cpu(x):
+    if available and isinstance(x, cupy.ndarray):
+        return x.get()
+    else:
+        return x
+
+
+def to_gpu(x, device=None):
+    if not available:
+        msg = 'CUDA environment is not correctly set up'
+        raise RuntimeError(msg)
+    with cupy.cuda.Device(device):
+        x_gpu = cupy.array(x)
+    return x_gpu
+
+
+def _to_cpu_variable(x):
+    x.data = to_cpu(x.data)
+
+
+def _to_gpu_variable(x):
+    x.data = to_gpu(x.data)
+
+
+def install_variable_cuda():
+    Variable.to_cpu = _to_cpu_variable
+    Variable.to_gpu = _to_gpu_variable
