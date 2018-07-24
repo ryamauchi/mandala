7764df75fe3b4f695ecc2b0e87c21ba3e4a15ed5
ryutaro-albert2005 <ryutaro_yamauchi@albert2005.co.jp>
2018-07-24 17:07:39 +0900

    add autodiff suppert to get_item

--- mandala/autodiff/__init__.py
+++ mandala/autodiff/__init__.py
@@ -1,6 +1,8 @@
-from mandala.autodiff import basic_math
 from mandala.autodiff import autodiff
+from mandala.autodiff import basic_math
+from mandala.autodiff import get_item
 
 
-basic_math.install_node_arithmetics()
 autodiff.install_node_backward()
+basic_math.install_node_arithmetics()
+get_item.install_node_get_item()
---
+++ mandala/autodiff/get_item.py
@@ -0,0 +1,33 @@
+import numpy
+from mandala.nodecore import Node
+from mandala.autodiff import autodiff
+
+
+def get_item_forward(x, slices):
+    return x[slices]
+
+
+def get_item_backward(x, slices, gy):
+    gx = numpy.zeros_like(x)
+    gx[slices] = gy
+    return gx
+
+
+class GetItem(autodiff.AutoDiff):
+    def forward(self, xs):
+        x, slices = xs
+        y = Node(get_item_forward, [x, slices])
+        return y
+
+    def backward(self, xs, gy):
+        x, slices = xs
+        gx = Node(get_item_backward, [x, slices, gy])
+        return gx,
+
+
+def get_item(x, slices):
+    return GetItem()([x, slices])
+
+
+def install_node_get_item():
+    Node.__getitem__ = get_item
