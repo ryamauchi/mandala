c1fcd2c019745c95ea7cdf954ffc7aabe3e8433e
ryutaro-albert2005 <ryutaro_yamauchi@albert2005.co.jp>
2018-07-25 17:04:24 +0900

    small update

--- mandala/autodiff/under_development/basic_math_ho.py
+++ mandala/autodiff/under_development/basic_math_ho.py
@@ -70,11 +70,11 @@ def rdiv(rhs, lhs):
 
 
 def floordiv(lhs, rhs):
-    raise NotImplementedError
+    raise NotImplementedError()
 
 
 def rfloordiv(rhs, lhs):
-    raise NotImplementedError
+    raise NotImplementedError()
 
 
 class Pow(autodiff.AutoDiff):
@@ -131,15 +131,15 @@ def neg(x):
 
 
 def absolute(a):
-    raise NotImplementedError
+    raise NotImplementedError()
 
 
 def matmul(lhs, rhs):
-    raise NotImplementedError
+    raise NotImplementedError()
 
 
 def rmatmul(lhs, rhs):
-    raise NotImplementedError
+    raise NotImplementedError()
 
 
 def install_node_arithmetics():
