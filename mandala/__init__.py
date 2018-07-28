cbcb0002b0ab0480d6b0f6fb14faf949f6e747d0
ryamauchi <ryutaro_yamauchi@albert2005.co.jp>
2018-07-28 06:43:16 +0000

    import Node, Variable here

--- mandala/__init__.py
+++ mandala/__init__.py
@@ -1,6 +1,8 @@
 from mandala import basic_math
 from mandala import get_item
 from mandala import cuda
+from mandala.nodecore import Node
+from mandala.nodecore import Variable
 
 
 basic_math.install_node_arithmetics()
