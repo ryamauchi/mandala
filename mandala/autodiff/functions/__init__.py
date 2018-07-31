c94d73552106ab0013d5e086fb5f1b79bafbed80
ryutaro-albert2005 <ryutaro_yamauchi@albert2005.co.jp>
2018-07-31 17:13:20 +0900

    update

--- mandala/autodiff/functions/__init__.py
+++ mandala/autodiff/functions/__init__.py
@@ -1,6 +1,26 @@
-from mandala.autodiff.functions import basic_math
-from mandala.autodiff.functions import get_item
-from mandala.autodiff.functions import reshape
-from mandala.autodiff.functions import relu
-from mandala.autodiff.functions import linear
-from mandala.autodiff.functions import convolution_2d
+from mandala.autodiff.functions import basic_math  # NOQA
+from mandala.autodiff.functions import get_item  # NOQA
+from mandala.autodiff.functions import linear  # NOQA
+from mandala.autodiff.functions import convolution_2d  # NOQA
+
+from basic_math import add  # NOQA
+from basic_math import mul  # NOQA
+from basic_math import sub  # NOQA
+from basic_math import div  # NOQA
+from basic_math import pow  # NOQA
+from basic_math import log  # NOQA
+from basic_math import neg  # NOQA
+from basic_math import absolute  # NOQA
+from basic_math import matmul  # NOQA
+
+from reshape import reshape  # NOQA
+from reshape import ReshapeFunction  # NOQA
+
+from relu import relu  # NOQA
+from relu import ReLUFunction  # NOQA
+
+from linear import Linear  # NOQA
+from linear import LinearFunction  # NOQA
+
+from convolution_2d import Convolution2D  # NOQA
+from convolution_2d import Convolution2DFunction  # NOQA
