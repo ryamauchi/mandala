61fac70fe0e329fb44ffe139f18dafdb6432d8c0
ryutaro-albert2005 <ryutaro_yamauchi@albert2005.co.jp>
2018-07-31 17:19:34 +0900

    bug fix

--- mandala/autodiff/functions/__init__.py
+++ mandala/autodiff/functions/__init__.py
@@ -1,26 +1,24 @@
 from mandala.autodiff.functions import basic_math  # NOQA
 from mandala.autodiff.functions import get_item  # NOQA
-from mandala.autodiff.functions import linear  # NOQA
-from mandala.autodiff.functions import convolution_2d  # NOQA
 
-from basic_math import add  # NOQA
-from basic_math import mul  # NOQA
-from basic_math import sub  # NOQA
-from basic_math import div  # NOQA
-from basic_math import pow  # NOQA
-from basic_math import log  # NOQA
-from basic_math import neg  # NOQA
-from basic_math import absolute  # NOQA
-from basic_math import matmul  # NOQA
+from mandala.autodiff.functions.basic_math import add  # NOQA
+from mandala.autodiff.functions.basic_math import mul  # NOQA
+from mandala.autodiff.functions.basic_math import sub  # NOQA
+from mandala.autodiff.functions.basic_math import div  # NOQA
+from mandala.autodiff.functions.basic_math import pow  # NOQA
+from mandala.autodiff.functions.basic_math import log  # NOQA
+from mandala.autodiff.functions.basic_math import neg  # NOQA
+from mandala.autodiff.functions.basic_math import absolute  # NOQA
+from mandala.autodiff.functions.basic_math import matmul  # NOQA
 
-from reshape import reshape  # NOQA
-from reshape import ReshapeFunction  # NOQA
+from mandala.autodiff.functions.reshape import reshape  # NOQA
+from mandala.autodiff.functions.reshape import ReshapeFunction  # NOQA
 
-from relu import relu  # NOQA
-from relu import ReLUFunction  # NOQA
+from mandala.autodiff.functions.relu import relu  # NOQA
+from mandala.autodiff.functions.relu import ReLUFunction  # NOQA
 
-from linear import Linear  # NOQA
-from linear import LinearFunction  # NOQA
+from mandala.autodiff.functions.linear import Linear  # NOQA
+from mandala.autodiff.functions.linear import LinearFunction  # NOQA
 
-from convolution_2d import Convolution2D  # NOQA
-from convolution_2d import Convolution2DFunction  # NOQA
+from mandala.autodiff.functions.convolution_2d import Convolution2D  # NOQA
+from mandala.autodiff.functions.convolution_2d import Convolution2DFunction  # NOQA
